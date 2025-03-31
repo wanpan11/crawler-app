8b6d593f34ea4d0349d50c089efa4117b65ef433
Wanpan <wanpan96@163.com>
2025-03-31 16:29:04 +0800

    feat: dist

--- src/api/account.ts
+++
@@ -1,6 +0,0 @@
-import request from "&src/utils/request";
-import type { AccountApi } from "../types/api";
-
-export const loginReq = (data: AccountApi.Login) => {
-  return request.send<{ token: string; userInfo: { account: string } }>("/login", "post", data);
-};
